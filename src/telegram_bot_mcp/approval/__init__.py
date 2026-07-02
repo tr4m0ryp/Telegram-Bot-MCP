3b73f7edef6793e84860e4e3dc794f00eba0c02a
Moussa Ouallaf <tramoryp@proton.me>
2026-07-02 23:26:47 +0200

    refactor(approval): split resolve_pending to cancel and delete

--- src/telegram_bot_mcp/approval/__init__.py
+++ src/telegram_bot_mcp/approval/__init__.py
@@ -1,14 +1,22 @@
 """Launch-approval request lifecycle and callback-data encoding."""
 
 from .callback import APPROVE, CANCEL, CallbackData, decode
-from .store import attach_message, create_pending, resolve_pending
+from .store import (
+    attach_message,
+    cancel_pending,
+    create_pending,
+    delete_pending,
+    valid_engagement_id,
+)
 
 __all__ = [
     "APPROVE",
     "CANCEL",
     "CallbackData",
     "attach_message",
+    "cancel_pending",
     "create_pending",
     "decode",
-    "resolve_pending",
+    "delete_pending",
+    "valid_engagement_id",
 ]
